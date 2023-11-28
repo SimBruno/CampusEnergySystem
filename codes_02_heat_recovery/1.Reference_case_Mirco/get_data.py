35c19b278465ced10a9a7f03001711de03417101
Mirco Pesenti Bucella <mirco.pesentibucella@epfl.ch>
2023-11-28 17:05:47 +0000

    Replace get_data.py

--- codes_02_heat_recovery/1.Reference_case_Mirco/get_data.py
+++ codes_02_heat_recovery/1.Reference_case_Mirco/get_data.py
@@ -6,9 +6,16 @@ from sklearn.cluster import KMeans
 import os
 
 
-path=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','..','codes_01_energy_demand','final_df_task1_medium.csv'))
-data=pd.read_csv(path,index_col=None)
-data.drop('Unnamed: 0',axis=1,inplace=True)
-data['Q_tot']=data.loc[:, (data.columns != 'hours') & (data.columns != 'Irr') & (data.columns != 'Temp')].sum(axis=1)
-print(data['hours'])
-print(data['Q_tot'])
+
+path=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','..','codes_01_energy_demand','Q_cluster_medium.csv'))
+data=pd.read_csv(path,index_col=None).rename(columns={'Unnamed: 0':'cluster'})
+print(data.sum(axis=1))
+path=os.path.abspath(os.path.join(os.path.dirname( __file__ ), '..','..','codes_01_energy_demand','clusters.csv'))
+cluster_data=pd.read_csv(path,index_col=None)
+print(cluster_data['hours'])
+
+
+#data.drop('Unnamed: 0',axis=1,inplace=True)
+#data['Q_tot']=data.loc[:, (data.columns != 'hours') & (data.columns != 'Irr') & (data.columns != 'Temp')].sum(axis=1)
+#print(data['hours'])
+#print(data['Q_tot'])
